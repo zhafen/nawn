452cc89302e4006055d6dba0c585a10c326c2b95
Zachary Hafen <zhafen@u.northwestern.edu>
2018-11-09 15:37:40 -0600

    Added a skeleton of desired functionality.

---
+++ nawn.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+'''@author: Zach Hafen
+@contact: zachary.h.hafen@gmail.com
+@status: Development
+'''
+
+########################################################################
+
+class Person( object ):
+
+    pass
+
+    # Desired functionality:
+    # Saving and loading
+    # Simple input of paper and associated key idea
+    
+    # Attributes to include:
+    # Papers
+    # Institution(s)
+    # Seen talks
+    # Output to node
+
+########################################################################
+
+class Paper( object ):
+
+    pass
+
+    # Desired functionality:
+    # Saving and loading
+    # Generation from bibtex
+    # Output to LaTeX
+    # Output to node (use NetworkX)
+
+    # Attributes to include:
+    # Identifying Name (Bibtex key if available)
+    # First Author
+    # Authors
+    # Journal
+    # Year
+    # Bibtex key
+    # Key ideas
+    # References
+
